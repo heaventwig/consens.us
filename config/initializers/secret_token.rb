91ed1739508fe31c083fef84466adb9b4f3848a6
Gavin White <gavin.white@alumni.reed.edu>
2011-10-31 16:14:58 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConsensUs::Application.config.secret_token = '1fa7dfbea4e64e8ff4b1f6993256371e8e0dee091ce1c581ad830f1b43413b723044753d71e352311cbcaf9f8573d8c2afc09f98e7d8033bbf989559354776b5'
